9fdbdd35be66b6123616a86836c3ec7ed8a7b5e7
LY_Matser <yan51055105@163.com>
2019-05-29 22:30:34 +0800

    Added price function

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+var dis = arguments[0];
+var time = arguments[1];
+var disp = 0.0;
+var timep = 0.0;
+var price = 0.0;
+if(dis<=2){
+   disp = 6;
+}
+if(dis>2&&dis<=8){
+   disp = 6+(dis-2)*0.8;
+}
+if(dis>8){
+  disp = 6+4.8+(dis-8)*1.2;
+}
+timep = time*0.25;
+
+price = Math.round(disp+timep);
+return price;
 };
